050a5e57e7c1120f053f88485d418748c47a9ba4
mac33 <mac33@a5s104mac23.fib.upc.es>
2016-01-29 14:26:39 +0100

    Initial Commit

---
+++ name2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  name2
+//
+//  Created by mac33 on 29/1/16.
+//  Copyright © 2016 mac33. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
